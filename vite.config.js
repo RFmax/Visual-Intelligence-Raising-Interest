7b93e809d1b0e30634baa3bb85fa5894779294e5
RFmax <Romando999@gmail.com>
2023-01-08 21:39:27 +0100

    gitignore and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react_tailwind_two/',
+  base: "/react_tailwind_two/",
   plugins: [react()],
 })
