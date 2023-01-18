de29968e9876db460f153b4acd223e807be29536
Romando <2065125heijnneman@zuyd.nl>
2023-01-18 12:17:24 +0100

    push live

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Application = () => {
-  return (
-    <div>Application</div>
-  )
-}
+const Application = () => (
+  <section>
+    <div>
+      <h2>Application</h2>
+    </div>
+  </section>
+)
 
 export default Application
